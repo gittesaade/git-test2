c1a26f0e0222d0effc489aa06edf3ae37fbe913c
tesaade <chrismaputri04@gmail.com>
2022-09-23 21:13:20 +0700

    menambah paragraf baru

--- index.php
+++ index.php
@@ -10,5 +10,6 @@
     <h3> Hello</h3>
     <p>
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae tempora sequi quia incidunt rerum! Tenetur odit blanditiis vel inventore, nobis ea cumque magni, ex assumenda, vero eos earum explicabo ab?</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus eveniet, distinctio reiciendis consequatur, est mollitia ratione quos maxime minima similique non facere repudiandae laboriosam! Deserunt suscipit a nostrum in laboriosam.</p>
 </body>
 </html>
